9ea87b84fef48edf586f6f4e4494a4b390740669
Betojr04 <betomasia12@gmail.com>
2025-10-02 12:40:08 -0700

    initialized maven gradle java springboot project in backend folder

---
+++ backend/src/main/java/com/investor_memo/demo/InvestorMemoApplication.java
@@ -0,0 +1,13 @@
+package com.investor_memo.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InvestorMemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InvestorMemoApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/investor_memo/demo/InvestorMemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.investor_memo.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InvestorMemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
